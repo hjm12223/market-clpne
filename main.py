be5a560692dc8345a3828e96dedc1fe6ff53429f
hjm12223 <gwa5068@gmail.com>
2023-07-05 13:44:21 +0900

    회원가입 로직 및 로그인 로직구현!
    프론트엔드 및 백엔드작업 signup login

---
+++ main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI, UploadFile, Form, Response
+from fastapi.staticfiles import StaticFiles
+from typing import Annotated
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+import sqlite3
+
+
+con = sqlite3.connect("db.db", check_same_thread=False)
+cur = con.cursor()
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL,
+            image BLOB,
+            price INTEGER NOT NULL,
+            discription TEXT,
+            place TEXT NOT NULL,
+            insertAt INTEGER NOT NULL
+            );
+            """)
+
+
+app = FastAPI()
+
+
+@app.post("/items")
+async def create_item(
+    image: UploadFile,
+    title: Annotated[str, Form()],
+    price: Annotated[int, Form()],
+    description: Annotated[str, Form()],
+    place: Annotated[str, Form()],
+    insertAt: Annotated[int, Form()],
+):
+    image_bytes = await image.read()
+    cur.execute(
+        f"""
+                INSERT INTO items(title, image, price, description, place,insertAt)
+                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}',{insertAt})
+                """
+    )
+    con.commit()
+    return "200"
+
+
+@app.get("/items")
+async def get_items():
+    con.row_factory = sqlite3.Row  # 컬럼명도 같이 가져옴
+    cur = con.cursor()
+    rows = cur.execute(
+        f""" 
+                       SELECT * FROM items;
+                       """
+    ).fetchall()
+
+    return JSONResponse(jsonable_encoder(dict(row) for row in rows))
+
+@app.post("/signup")
+def signup(
+    id: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    name: Annotated[str, Form()],
+    email: Annotated[str, Form()]
+):
+    cur.execute(f"""
+                INSERT INTO users(id, name, email, password)
+                VALUES ('{id}', '{name}', '{email}', '{password}')
+                """)    
+    con.commit()
+    return '200'
+
+
+@app.get("/images/{item_id}")
+async def get_images(item_id):
+    cur = con.cursor()
+    image_bytes = cur.execute(
+        f"""
+        SELECT image from items WHERE id = {item_id}
+        """
+    ).fetchone()[0]
+    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
+
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
