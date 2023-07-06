10e7aa48df4347a0d9cafd0e35abc0b8a9a436d4
hjm12223 <gwa5068@gmail.com>
2023-07-06 15:35:37 +0900

    svelte, vite 로 바꾼 후 firebase deploy setting

---
+++ src/pages/Login.svelte
@@ -0,0 +1,21 @@
+<body>
+  <form id="login-form" action="/login" method="POST">
+    <div>로그인</div>
+    <div>
+      <label for="id">아이디</label>
+      <input type="text" id="id" name="id" required />
+    </div>
+    <div>
+      <label for="password">패스워드</label>
+      <input type="password" id="password" name="password" required />
+    </div>
+    <div>
+      <button type="submit">로그인</button>
+    </div>
+    <div id="info" />
+    <script src="login.js"></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.min.js"
+    ></script>
+  </form></body
+>
---
+++ src/pages/Main.svelte
@@ -0,0 +1,64 @@
+<script>
+  let hour = new Date().getHours();
+  let min = new Date().getMinutes();
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}</div>
+    <div class="info-bar__icons">
+      <img src="assets/chart-bar.svg" alt="" />
+      <img src="assets/wifi-bar.svg" alt="" />
+      <img src="assets/battery.svg" alt="" />
+    </div>
+  </div>
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div>역삼1동</div>
+      <div class="menu-bar__location-icon">
+        <img src="assets/arrow.svg" alt="" />
+      </div>
+    </div>
+    <div class="menu-bar__icons">
+      <img src="assets/search.svg" alt="" />
+      <img src="assets/menu.svg" alt="" />
+      <img src="assets/alert.svg" alt="" />
+    </div>
+  </div>
+</header>
+<main>
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+<footer>
+  <div class="footer-block">
+    <div class="footer-icons">
+      <img src="assets/home.svg" alt="" class="footer-icons__image" />
+      <div class="footer-icons__desc">홈</div>
+    </div>
+
+    <div class="footer-icons">
+      <img src="assets/document.svg" alt="" class="footer-icons__image" />
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+
+    <div class="footer-icons">
+      <img src="assets/location.svg" alt="" class="footer-icons__image" />
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+
+    <div class="footer-icons">
+      <img src="assets/chat.svg" alt="" class="footer-icons__image" />
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+
+    <div class="footer-icons">
+      <img src="assets/user.svg" alt="" class="footer-icons__image" />
+      <div class="footer-icons__desc">나의 당근</div>
+    </div>
+  </div>
+</footer>
+
+<!-- <div class="media-info-msg">화면 사이즈를 줄여주세요.</div> -->
+
+<style>
+</style>
