e929aff11ed420c7d3dfcc8cd877e45b3fa0958a
hjm12223 <gwa5068@gmail.com>
2023-07-06 17:51:48 +0900

    onMount

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import App from './App.svelte'
+import App from './App.svelte';
+import "../firebase.js";
+
 
 const app = new App({
   target: document.getElementById('app'),
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,27 +1,68 @@
+<script>
+  import { getDatabase, ref, push } from "firebase/database";
+  import { prevent_default } from "svelte/internal";
+  import Footer from "../components/Footer.svelte";
+
+  let title;
+  let price;
+  let description;
+  let place;
+
+  async function writeUserData() {
+    const db = getDatabase();
+    push(ref(db, "items/"), {
+      title,
+      price,
+      description,
+      place,
+    });
+    alert("글쓰기 완료되었습니다.");
+    window.location.hash = "/";
+  }
+</script>
+
 <body>
-  <form id="write-form">
-    <div>
+  <form id="write-form" on:submit|preventDefault={writeUserData}>
+    <!-- <div>
       <label for="">이미지</label>
       <input type="file" id="image" name="image" />
-    </div>
+    </div> -->
     <div>
       <label for="">제목</label>
-      <input type="text" id="title" name="title" />
+      <input type="text" id="title" name="title" bind:value={title} />
     </div>
     <div>
       <label for="">가격</label>
-      <input type="number" id="price" name="price" />
+      <input type="number" id="price" name="price" bind:value={price} />
     </div>
     <div>
       <label for="">설명</label>
-      <input type="text" id="description" name="description" />
+      <input
+        type="text"
+        id="description"
+        name="description"
+        bind:value={description}
+      />
     </div>
     <div>
       <label for="">장소</label>
-      <input type="text" id="place" name="place" />
+      <input type="text" id="place" name="place" bind:value={place} />
     </div>
     <div>
-      <button type="submit">제출</button>
+      <button class="write-button" type="submit">제출</button>
     </div>
   </form>
 </body>
+
+<Footer location="write" />
+
+<style>
+  .write-button {
+    background-color: tomato;
+    margin: 10px;
+    border-radius: 10px;
+    padding: 5px 12px 5px 12px;
+    color: white;
+    cursor: pointer;
+  }
+</style>
